e8f0812c56217ecb33dd6ba2e73333bfcd598a29
mkashif1000 <mkashif.work@gmail.com>
2024-12-16 17:52:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let slider2 = document.querySelector (".slider2");
 let slider3 = document.querySelector (".slider3");
 
 slider1.addEventListener("mouseover" , () => {
-    img.src = "Images/Bottle_Orange.png";
+    img.src = "/Bottle_Orange.png";
 })
 
 slider2.addEventListener("mouseover" , () => {
-    img.src = "Images/Bottle_Black.png";
+    img.src = "/Bottle_Black.png";
 })
 
 slider3.addEventListener("mouseover" , () => {
-    img.src = "Images/Bottle_Blue.png";
+    img.src = "/Bottle_Blue.png";
 })
